83b5a37af3ddf6d86eff4b7c0f43ca1748af1d23
Mohamed Khaled <mohamedkhalidshaaban@gmail.com>
2026-08-27 17:14:38 +0400

    add import salary, projects routes

--- server/controllers/import.controller.ts
+++ server/controllers/import.controller.ts
@@ -3,6 +3,10 @@ import fs from "fs/promises";
 
 import { parseTimesheet } from "../parsers/timesheet.parser.js";
 import { importTimesheet } from "../services/import/timesheet-import.service.js";
+import { importSalaries } from "../services/import/salary-import.service.js";
+import { importProjects } from "../services/import/project-import.service.js";
+import { parseSalary } from "../parsers/salary.parser.js";
+import { parseProjects } from "../parsers/project.parser.js";
 
 export async function uploadTimesheet(
   req: Request,
@@ -27,3 +31,43 @@ export async function uploadTimesheet(
     await fs.unlink(req.file.path).catch(() => {});
   }
 }
+
+export async function uploadSalary(
+    req: Request,
+    res: Response
+  ) {
+    if (!req.file) {
+      return res.status(400).json({
+        message: "Salary file is required",
+      });
+    }
+  
+    const salaries = parseSalary(req.file.path);
+  
+    await importSalaries(salaries);
+  
+    return res.json({
+      message: "Salary imported successfully",
+      rows: salaries.length,
+    });
+  }
+
+  export async function uploadProjects(
+    req: Request,
+    res: Response
+  ) {
+    if (!req.file) {
+      return res.status(400).json({
+        message: "Project file is required",
+      });
+    }
+  
+    const projects = parseProjects(req.file.path);
+  
+    await importProjects(projects);
+  
+    return res.json({
+      message: "Projects imported successfully",
+      rows: projects.length,
+    });
+  }
--- server/routes/import.routes.ts
+++ server/routes/import.routes.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 
 import { upload } from "../middleware/upload.middleware.js";
-import { uploadTimesheet } from "../controllers/import.controller.js";
+import { uploadProjects, uploadSalary, uploadTimesheet } from "../controllers/import.controller.js";
 
 const router = Router();
 
@@ -10,5 +10,17 @@ router.post(
   upload.single("file"),
   uploadTimesheet
 );
+
+router.post(
+    "/salary",
+    upload.single("file"),
+    uploadSalary
+  );
+  
+  router.post(
+    "/projects",
+    upload.single("file"),
+    uploadProjects
+  );
 
 export default router;
